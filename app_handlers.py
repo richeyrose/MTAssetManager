c923fe498f3a3f6f866db45bd5fcdc8041665651
Richard Rose <richard.rose@modmodterrain.com>
2020-11-01 18:35:58 +0000

    added set_asset_desc_filepaths() function
    We now write asset filepaths to asset description .json files on first load

--- app_handlers.py
+++ app_handlers.py
@@ -5,16 +5,56 @@ from bpy.app.handlers import persistent
 from .system import get_addon_path
 from .preferences import get_prefs
 from .categories import load_categories
+from .utils import dedupe
+
 
 def mt_am_initialise_on_activation(dummy):
+    prefs = get_prefs()
     bpy.app.handlers.depsgraph_update_pre.remove(mt_am_initialise_on_activation)
-    create_propertes()
+    props = bpy.context.scene.mt_am_props
+    create_properties()
+    asset_types = ['objects', 'collections', 'materials']
+    set_asset_desc_filepaths(prefs.default_assets_path, asset_types)
+    load_asset_descriptions(props)
 
 @persistent
 def mt_am_initialise_on_load(dummy):
-    create_propertes()
+    props = bpy.context.scene.mt_am_props
+    create_properties()
+    load_asset_descriptions(props)
 
-def create_propertes():
+def set_asset_desc_filepaths(assets_path, asset_types):
+    """Stores the path to the associated asset type in asset description .json file
+
+    Args:
+        assets_path (str): path to assets folder
+        asset_types (str): type of asset
+    """
+    for asset_type in asset_types:
+        json_file = os.path.join(
+            assets_path,
+            "data",
+            asset_type + ".json")
+
+        if os.path.exists(json_file):
+            with open(json_file) as read_file:
+                assets = json.load(read_file)
+
+            for asset in assets:
+                asset["FilePath"] = os.path.join(
+                    assets_path,
+                    asset_type,
+                    asset["FileName"])
+                asset["PreviewImagePath"] = os.path.join(
+                    assets_path,
+                    asset_type,
+                    asset["PreviewImageName"])
+
+            with open(json_file, "w") as write_file:
+                json.dump(assets, write_file, indent=4)
+
+
+def create_properties():
     """Create custom properties."""
     prefs = get_prefs()
     props = bpy.context.scene.mt_am_props
@@ -31,12 +71,11 @@ def create_propertes():
     props.categories = categories  # all categories
     props.child_cats = categories  # child categories of active category
 
-    load_asset_descriptions(props)
 
 def load_missing_preview_image():
     prefs = get_prefs()
     no_image_path = os.path.join(
-        prefs.assets_path,
+        prefs.default_assets_path,
         "misc",
         "no_image.png"
     )
@@ -45,11 +84,13 @@ def load_missing_preview_image():
         return bpy.data.images.load(no_image_path, check_existing=True)
     return None
 
+
 def load_asset_descriptions(props):
     """Load asset descriptions from .json file."""
     props.objects = load_object_descriptions()
     props.collections = load_collection_descriptions()
     props.materials = load_material_descriptions()
+
 
 def load_collection_descriptions():
     """Load Collection descriptions from .json file.
@@ -57,14 +98,12 @@ def load_collection_descriptions():
     Returns:
         [list]: List of Collection asset descriptions
     """
-
-    addon_path = get_addon_path()
-    props = bpy.context.scene.mt_am_props
+    prefs = get_prefs()
     default_collections = []
 
     # load default collections bundled with asset manager
     json_path = os.path.join(
-        addon_path,
+        prefs.default_assets_path,
         "data",
         "collections.json")
 
@@ -82,13 +121,12 @@ def load_material_descriptions():
     Returns:
         [list]: list of Material asset descriptions
     """
-    addon_path = get_addon_path()
-    props = bpy.context.scene.mt_am_props
+    prefs = get_prefs()
     default_materials = []
 
-    # load default materials bundled with asset manager
+    # load materials bundled with asset manager
     json_path = os.path.join(
-        addon_path,
+        prefs.default_assets_path,
         "data",
         "materials.json")
 
@@ -107,22 +145,32 @@ def load_object_descriptions():
     Returns:
         [list]: list of Object asset descriptions
     """
-    addon_path = get_addon_path()
-    props = bpy.context.scene.mt_am_props
-    default_objects = []
+    prefs = get_prefs()
+    ob_descs = []
 
     # load default objects bundled with asset manager
     json_path = os.path.join(
-        addon_path,
+        prefs.default_assets_path,
         "data",
         "objects.json")
 
     if os.path.exists(json_path):
         with open(json_path) as json_file:
-            default_objects = json.load(json_file)
+            ob_descs = json.load(json_file)
 
-    # TODO load user objects
-    return default_objects
+    # load user objects
+    json_path = os.path.join(
+        prefs.user_assets_path,
+        "data",
+        "objects.json")
+
+    if os.path.exists(json_path):
+        with open(json_path) as json_file:
+            ob_descs.extend(json.load(json_file))
+
+    return list(dedupe(ob_descs, key=lambda d: d['Slug']))
+
+
 
 bpy.app.handlers.depsgraph_update_pre.append(mt_am_initialise_on_activation)
 bpy.app.handlers.load_post.append(mt_am_initialise_on_load)
