79d2425a7d0a6fbd17aa9680e1ed4cfa2772d067
Richard Rose <richard.rose@modmodterrain.com>
2020-11-21 16:32:24 +0000

    Can now spawn collections from asset manager

--- spawn.py
+++ spawn.py
@@ -1,6 +1,5 @@
 import os
 import bpy
-from mathutils import kdtree
 from .raycast import mouse_raycast, floor_raycast
 from .utils import material_is_unique, find_vertex_group_of_face, assign_mat_to_vert_group, get_material_index
 
@@ -8,6 +7,7 @@ from .utils import material_is_unique, find_vertex_group_of_face, assign_mat_to_
 def spawn_object(self, context, asset, x, y, op):
     coords = (x, y)
     obj = append_object(context, asset, op)
+
     if not obj:
         op.report({'WARNING'}, "Asset not found!")
         return False
@@ -34,8 +34,37 @@ def spawn_object(self, context, asset, x, y, op):
     # push an undo action to the stack
     bpy.ops.ed.undo_push()
 
+
 def spawn_collection(self, context, asset, x, y, op):
-    pass
+    coords = (x, y)
+    collection, root_object = append_collection(context, asset, op)
+
+    if not collection:
+        op.report({'WARNING'}, "Asset not found!")
+        return False
+
+    # check if there is an object under the mouse.
+    hit, location, normal, rotation, face_index, hit_obj, matrix = mouse_raycast(context, coords)
+
+    # if we've not hit an object we'll spawn on an imaginary plane at z = 0
+    if not hit:
+        hit, location, normal, rotation, face_index, hit_obj, matrix = floor_raycast(context, coords)
+
+    # deselect any currently selected objects
+    for obj in context.selected_objects:
+        obj.select_set(False)
+
+    # select the root object
+    bpy.context.view_layer.objects.active = root_object
+    root_object.select_set(True)
+
+    # set root object location and rotation to hit point
+    root_object.location = location
+    root_object.rotation_euler = rotation
+
+    # push an undo action to the stack
+    bpy.ops.ed.undo_push()
+
 
 def spawn_material(self, context, asset, x, y, op):
     coords = (x, y)
@@ -139,7 +168,8 @@ def append_object(context, asset, op, link=False):
                 asset_found = True
 
         if asset_found:
-            obj = data_to.objects[0]  # the object that corresponds to the asset['Name']
+            # the object that corresponds to the asset['Slug']
+            obj = data_to.objects[0]
 
             # link our imported object to current collection
             context.collection.objects.link(obj)
@@ -151,6 +181,8 @@ def append_object(context, asset, op, link=False):
             # set a fake user for our secondary objects
             for ob_name in new_obs:
                 ob = bpy.data.objects[ob_name]
+                # TODO We can probably get rid of this check and just make sure every new object
+                # has a fake user
                 if ob != obj:
                     ob.use_fake_user = True
 
@@ -180,4 +212,73 @@ def append_object(context, asset, op, link=False):
             return obj
 
     return False
+
+
+def append_collection(context, asset, op, link=False):
+    filepath = asset["FilePath"]
+    # used to ensure we only add unique materials
+    existing_mats = bpy.data.materials.keys()
+
+    # used to ensure we set add a fake user on secondary objects, like those referred
+    # to in modifiers, if they are added to the scene. If we don't do this then when
+    # we resave an object it won't save the associated objects (as of 2.82a)
+    existing_obs = bpy.data.objects.keys()
+
+    if os.path.exists(filepath) and os.path.isfile(filepath):
+        asset_found = False
+
+        # load asset
+        with bpy.data.libraries.load(filepath, link=link) as (data_from, data_to):
+            if asset['Slug'] in data_from.collections:
+                data_to.collections = [asset['Slug']]
+                if asset['RootObject'] in data_from.objects:
+                    data_to.objects = [asset['RootObject']]
+                asset_found = True
+
+        if asset_found:
+            # the collection that corresponds to the asset['Slug']
+            collection = data_to.collections[0]
+            root_object = data_to.objects[0]
+
+            # link collection to scene
+            context.scene.collection.children.link(collection)
+
+            # we need to ensure that any other objects that have been added as a side
+            # effect have a fake user set otherwise things start breaking when we resave
+            # objects and collection
+            updated_obs = bpy.data.objects.keys()
+
+            new_obs = (set(existing_obs) | set(updated_obs)) - (set(existing_obs) & set(updated_obs))
+            for ob_name in new_obs:
+                ob = bpy.data.objects[ob_name]
+                ob.use_fake_user = True
+
+            # get a set of new materials that were added on import
+            updated_mats = bpy.data.materials.keys()
+            new_mats = (set(existing_mats) | set(updated_mats)) - (set(existing_mats) & set(updated_mats))
+
+            # check if newly added materials are unique
+            for mat in new_mats:
+                materials = [material for material in bpy.data.materials if mat != material]
+                unique, matched_material = material_is_unique(bpy.data.materials[mat], materials)
+
+                # if not unique replace with matched material
+                if not unique:
+                    for obj in new_obs:
+                        for slot in obj.material_slots:
+                            if slot.material.name == mat:
+                                slot.material = matched_material
+
+                    # remove duplicate material
+                    bpy.data.materials.remove(bpy.data.materials[mat])
+
+            # rename collection to pretty name
+            collection.name = asset["Name"]
+            op.report({'INFO'}, collection.name + ' added to scene.')
+            return collection, root_object
+    return
+
+
+
+
 
