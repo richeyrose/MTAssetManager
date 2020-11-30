745f1966388bf824c5a72d513e7a2e55c6dd153e
Richard Rose <richard.rose@modmodterrain.com>
2020-11-30 09:11:45 +0000

    Removed alternative delete asset operator. We now always delete selected assets

--- delete_from_library.py
+++ delete_from_library.py
@@ -7,11 +7,11 @@ from .preferences import get_prefs
 
 
 class MT_OT_AM_Delete_Selected_Assets_from_Library(Operator):
-    """Operator that deletes all assets selected in the asset bar from the MakeTile library and optionally from disk."""
+    """Delete all selected assets from the MakeTile library and optionally from disk."""
 
     bl_idname = "object.delete_selected_assets_from_library"
     bl_label = "Delete Selected Assets"
-    bl_description = "deletes all assets selected in the asset bar from the MakeTile library and optionally from disk"
+    bl_description = "Delete all selected assets from the MakeTile library and optionally from disk."
 
     delete_from_disk: BoolProperty(
         name="Also Delete From Disk?",
@@ -73,66 +73,3 @@ class MT_OT_AM_Delete_Selected_Assets_from_Library(Operator):
         layout.prop(self, 'delete_from_disk')
 
 
-class MT_OT_AM_Delete_Asset_from_Library(Operator):
-    """Operator that deletes asset from MakeTile library and optionally from disk."""
-    bl_idname = "object.delete_asset_from_library"
-    bl_label = "Delete Asset from Library"
-    bl_description = "Deletes asset from the MakeTile Library and optionally from disk"
-
-    delete_from_disk: BoolProperty(
-        name="Also Delete From Disk?",
-        default=False)
-
-    def execute(self, context):
-        prefs = get_prefs()
-        props = context.scene.mt_am_props
-        asset_desc = props.current_asset_desc
-
-        # get in memory list of asset_descs
-        asset_descs = getattr(props, asset_desc["Type"].lower())
-
-        # remove item from in memory list
-        asset_descs.remove(asset_desc)
-
-        # overwrite .json file with modified list
-        json_file = os.path.join(
-            prefs.user_assets_path,
-            "data",
-            asset_desc["Type"].lower() + ".json")
-
-        if os.path.exists(json_file):
-            with open(json_file, "w") as write_file:
-                json.dump(asset_descs, write_file, indent=4)
-        else:
-            os.makedirs(json_file)
-            with open(json_file, "w") as write_file:
-                json.dump(asset_descs, write_file, indent=4)
-
-        # delete preview image
-        if os.path.exists(asset_desc["PreviewImagePath"]):
-            os.remove(asset_desc["PreviewImagePath"])
-
-        # delete asset file
-        if self.delete_from_disk:
-            if os.path.exists(asset_desc["FilePath"]):
-                os.remove(asset_desc["FilePath"])
-
-        # raise flag to update asset bar
-        props.assets_updated = True
-
-        return {'FINISHED'}
-
-    def invoke(self, context, event):
-        return context.window_manager.invoke_props_dialog(self)
-
-    def draw(self, context):
-        """Draw confirmation dialog.
-
-        Args:
-            context (bpy.context): context
-        """
-        props = context.scene.mt_am_props
-        layout = self.layout
-        layout.label(text="Delete " + props.current_asset_desc['Name'] + "?")
-        layout.label(text="Warning! Cannot be Undone!")
-        layout.prop(self, 'delete_from_disk')
