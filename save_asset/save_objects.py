0bc4a515aabe076e43668a4502978067430f0888
Richard Rose <richard.rose@modmodterrain.com>
2020-12-29 10:36:10 +0000

    modified save objects so assets are now marked as blender assets for internal browser

--- save_asset/save_objects.py
+++ save_asset/save_objects.py
@@ -5,10 +5,12 @@ from bpy.props import StringProperty
 from .add_to_library import (
     draw_save_props_menu,
     check_category_type,
-    add_asset_to_library)
+    add_asset_to_library,
+    construct_asset_description,
+    save_as_blender_asset)
 from ..preferences import get_prefs
 from .preview_rendering import render_object_preview
-
+from ..utils import tagify
 
 class MT_OT_AM_Add_Multiple_Objects_To_Library(Operator):
     """Add all selected mesh objects to the MakeTile Library."""
@@ -16,6 +18,32 @@ class MT_OT_AM_Add_Multiple_Objects_To_Library(Operator):
     bl_idname = "object.add_selected_objects_to_library"
     bl_label = "Add selected objects to library"
     bl_description = "Adds selected objects to the MakeTile Library"
+
+    Description: StringProperty(
+        name="Description",
+        default=""
+    )
+
+    URI: StringProperty(
+        name="URI",
+        default=""
+    )
+
+    Author: StringProperty(
+        name="Author",
+        default=""
+    )
+
+    License: StringProperty(
+        name="License",
+        default="All Rights Reserved"
+    )
+
+    Tags: StringProperty(
+        name="Tags",
+        description="Comma seperated list",
+        default=""
+    )
 
     @classmethod
     def poll(cls, context):
@@ -33,34 +61,27 @@ class MT_OT_AM_Add_Multiple_Objects_To_Library(Operator):
         props = context.scene.mt_am_props
         prefs = get_prefs()
         assets_path = prefs.user_assets_path
+        asset_type = "OBJECTS"
+        tags = tagify(self.Tags)
+
+        kwargs = {
+            "Description": self.Description,
+            "URI": self.URI,
+            "Author": self.Author,
+            "License": self.License,
+            "Tags": tags}
+
         scene_path = os.path.join(
             prefs.default_assets_path,
             "previews",
             "preview_scenes.blend")
-        asset_type = "OBJECTS"
-
-        # check if we're in a sub category that contains assets of the correct type.
-        # If not add the object to the root category for its type
-        if props.active_category is None:
-            category = asset_type.lower()
-        else:
-            category = check_category_type(props.active_category, asset_type)
-
-        kwargs = {
-            "Description": "",
-            "URI": "",
-            "Author": "",
-            "License": ""}
 
         for obj in obs:
-            asset_desc = add_asset_to_library(
-                self,
-                context,
+            asset_desc = construct_asset_description(
                 props,
-                obj,
+                asset_type,
                 assets_path,
-                "OBJECTS",
-                category,
+                obj,
                 **kwargs)
 
             render_object_preview(
@@ -71,8 +92,29 @@ class MT_OT_AM_Add_Multiple_Objects_To_Library(Operator):
                 prefs.preview_scene,
                 obj)
 
+            # save asset data for Blender asset browser
+            if hasattr(obj, 'asset_data'):
+                save_as_blender_asset(obj, asset_desc, tags)
+
+            # add the asset to the MakeTile library
+            add_asset_to_library(
+                self,
+                context,
+                obj,
+                asset_type,
+                asset_desc)
+
         props.assets_updated = True
+
         return {'FINISHED'}
+
+    def invoke(self, context, event):
+        """Call when operator invoked from UI."""
+        return context.window_manager.invoke_props_dialog(self)
+
+    def draw(self, context):
+        """Draw popup property menu."""
+        draw_save_props_menu(self, context)
 
 
 class MT_OT_AM_Add_Active_Object_To_Library(Operator):
@@ -125,29 +167,20 @@ class MT_OT_AM_Add_Active_Object_To_Library(Operator):
         prefs = get_prefs()
         assets_path = prefs.user_assets_path
         asset_type = "OBJECTS"
-
-        # check if we're in a sub category that contains assets of the correct type.
-        # If not add the object to the root category for its type
-        if props.active_category is None:
-            category = asset_type.lower()
-        else:
-            category = check_category_type(props.active_category, asset_type)
+        tags = tagify(self.Tags)
 
         kwargs = {
             "Description": self.Description,
             "URI": self.URI,
             "Author": self.Author,
-            "License": self.License}
+            "License": self.License,
+            "Tags": tags}
 
-        asset_desc = add_asset_to_library(
-            self,
-            context,
+        asset_desc = construct_asset_description(
             props,
-            obj,
-            assets_path,
             asset_type,
-            category,
-            self.Tags,
+            assets_path,
+            obj,
             **kwargs)
 
         scene_path = os.path.join(
@@ -162,6 +195,17 @@ class MT_OT_AM_Add_Active_Object_To_Library(Operator):
             scene_path,
             prefs.preview_scene,
             obj)
+
+        # save asset data for Blender asset browser
+        if hasattr(obj, 'asset_data'):
+            save_as_blender_asset(obj, asset_desc, tags)
+
+        add_asset_to_library(
+            self,
+            context,
+            obj,
+            asset_type,
+            asset_desc)
 
         props.assets_updated = True
 
